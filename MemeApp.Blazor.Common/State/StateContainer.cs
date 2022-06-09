b2eaff8935905f7570132b6e7cabe6758c2816d7
George Findulov <george.findulov@outlook.com>
2022-06-09 21:06:43 +0300

    Add state management example..

---
+++ MemeApp.Blazor.Common/State/StateContainer.cs
@@ -0,0 +1,21 @@
+﻿namespace MemeApp.Blazor.Common.State
+{
+    public class StateContainer
+    {
+        private int memeCount;
+
+        public int MemeCount
+        {
+            get => memeCount;
+            set
+            {
+                memeCount = value;
+                NotifyStateChanged();
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
--- MemeApp.Blazor/Program.cs
+++ MemeApp.Blazor/Program.cs
@@ -1,5 +1,6 @@
 using MemeApp.Blazor;
 using MemeApp.Blazor.Common.Services;
+using MemeApp.Blazor.Common.State;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,6 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped<IMemeApiService, MemeApiService>();
+builder.Services.AddScoped<IMemeApiService, MemeApiService>(); 
+builder.Services.AddScoped<StateContainer>();
 
 await builder.Build().RunAsync();
